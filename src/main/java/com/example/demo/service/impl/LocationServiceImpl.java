30b5b5b3403d00d09659fb6b0f77d2520b7cdd24
bala-amypo <bala@amypo.in>
2025-12-15 16:42:44 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/LocationServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/LocationServiceImpl.java
@@ -1,9 +1,10 @@
 package com.example.demo.service.impl;
 
-/**
- * LocationServiceImpl
- */
-public class LocationServiceImpl {
+import org.springframework.stereotype.Service;
+
+import com.example.demo.service.LocationService;
+@Service
+public class LocationServiceImpl implements LocationService{
 
     
 }
