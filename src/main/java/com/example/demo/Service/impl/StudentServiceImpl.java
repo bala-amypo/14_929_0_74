fabb559078e7123bdcc8559d126d3348a956d416
bala-amypo <bala@amypo.in>
2025-12-16 18:21:34 +0000

    User commit

---
+++ src/main/java/com/example/demo/Service/impl/StudentServiceImpl.java
@@ -0,0 +1,49 @@
+package com.example.demo.Service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Entity.StudentEntity;
+import com.example.demo.Repository.StudentRepo;
+import com.example.demo.Service.StudentService;
+
+@Service
+public class StudentServiceImpl  implements StudentService { 
+//save
+//findById()
+//findAll();
+//existsById()
+//deleteById()
+
+@Autowired StudentRepo repo;
+
+@Override
+ public StudentEntity postdata(StudentEntity stu){
+   return repo.save(stu);
+ }
+ @Override
+    public List<StudentEntity> getdata(){
+     return repo.findAll();
+    }
+    @Override
+   public  StudentEntity updatedata(int id,StudentEntity std){
+     if(repo.existsById(id)){
+        std.setId(id);
+        return repo.save(std);
+
+     }
+     return null;
+
+    }
+    @Override
+   public  String deletedata(int id){
+    repo.deleteById(id);
+    return "Deleted Sucessfully";
+
+    }
+
+
+    
+}
